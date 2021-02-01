25b24b04be213191d208d279edb76c5af3fbe367
Ross Leonard <77854033+Ross-Leonard@users.noreply.github.com>
2021-02-01 12:08:38 -0600

    Program that does a multitude of Calculations
    This program finds the greatest common divisor.
    Program prints array in reverse.
    Some other things too.

--- Source.cpp
+++ Source.cpp
@@ -1,271 +1,181 @@
 //John Ross Leonard
 //CS 372
-//Infix to Postfix
+//Number 2 on worksheet, multiple programs
 
 #include <iostream>
-#include <cstring>
-#include <string>
-#include <cmath>
-#include <fstream>
-
 using namespace std;
 
-class List
-{
-private:
-    int mySize;   
-    int pos;
-public:    
-    int CAPACITY;
-    int* arr;
-    int currentPos, size1;    
-};
 
-//stack for postfix
-class Stack
+//Write a recursive function that returns the number of digits in a nonnegative integer
+int countdigits(int num)
 {
-private:
-    char* list;
-    int size;
-    int index;	
-
-public:
-    Stack(int s)
-    {
-        size = s;
-        list = new char[s];
-        index = -1;
-    }
-    void push(char p)
-    {
-        index++;
-        list[index] = p;
-    }	
-    char peek()
-    {
-        char ret = ' ';
-        if (index > -1)
-        {
-            ret = list[index];
-        }
-        return(ret);
-    }
-    char pop()
-    {
-        char ret = ' ';
-        if (index > -1)
-        {
-            ret = list[index];
-            index--;
-        }
-        return(ret);
-    }   
-    bool isEmpty()
-    {
-        return(index == -1);
-    }	
-};
-
-//evaluation stack
-class StackInt
+	static int count = 0;
+	if (num > 0)
+	{
+		count++;
+		countdigits(num / 10);
+	}
+	else
+	{
+		return count;
+	}
+}
+//Write a recursive function printReverse() that displays an integers digits in reverse order
+int printReverse(int n)
 {
-private:
-    int* list;
-    int size;
-    int index;
-public:
-    StackInt(int s)
-    {
-        size = s;
-        list = new int[s];
-        index = -1;
-    }
-    void push(int p)
-    {
-        index++;
-        list[index] = p;
-    }
-    int peek()
-    {
-        int ret = ' ';
-        if (index > -1)
-        {
-            ret = list[index];
-        }
-        return(ret);
-    }
-    int pop()
-    {
-        int ret = ' ';
-        if (index > -1)
-        {
-            ret = list[index];
-            index--;
-        }
-        return(ret);
-    }
-    bool isEmpty()
-    {
-        return(index == -1);
-    }
-    int totals()
-    {
-        return(index + 1);
-    }
-};
-
-int preoperation(char ch)
+	int rev = 0;
+	int modding;
+	if (n == 0)	
+		return n;	
+	else
+	{
+		modding = n % 10;		
+		cout << modding <<" ";
+		printReverse(n/10);	
+	}	
+}
+//Write recursive definitions of the functions whose prototypes are given in Exercises 27-29
+//27
+void reverseArray(int array[], int first, int last)
 {
-    if (ch == '^')
-    {
-        return 3;
-    }
-    else
-    {
-        if (ch == '*' || ch == '/')
-        {
-            return 2;
-        }
-        else
-        {
-            if (ch == '+' || ch == '-')
-            {
-                return 1;
-            }
-            else
-            {
-                return -1;
-            }
-        }
-    }
+	int i = 0;
+	int temp;
+	if (first > last)
+	{
+		cout << array[last];
+		return;		
+	}
+	else
+	{
+		temp = array[first];
+		array[first] = array[last];
+		array[last] = temp;		
+		reverseArray(array, first + 1, last - 1);		
+	}
+	cout << array[last];	
 }
 
-string intopost(string s)
-{   
-    Stack stack(100);    
-    stack.push('N');
-    int len = s.length();
-    string ret = " ";
-    for (int i = 0; i < len; i++)
-    {
-        if (s.at(i) >= '0' && s.at(i) <= '9')
-        {
-            ret += s.at(i);
-        }
-        else
-        {
-            if (s.at(i)=='(')
-            {
-                stack.push('(');
-            }
-            else
-            {
-                if (s.at(i) == ')')
-                {
-                    while (stack.peek() != 'N' && stack.peek() != '(')
-                    {
-                        char c = stack.pop();
-                        ret += c;
-                    }
-                    if (stack.peek() == '(')
-                    {
-                        stack.pop();
-                    }
-                }
-                else
-                {
-                    while (stack.peek() != 'N' && preoperation(s.at(i)) <= preoperation(stack.peek()))
-                    {
-                        char c = stack.pop();
-                        ret += c;
-                    }
-                    stack.push(s.at(i));
-                }
-            }
-        }
-    }
-    while (stack.peek() != 'N')
-    {
-        ret += stack.pop();
-    }
-    return(ret);
-}
-
-int evaluatepost(string s)
+////Write recursive definitions of the functions whose prototypes are given in Exercises 27-29
+//28
+int sumArray(int array[], int n)
 {
-    int len = s.length();
-    int ret = 0;
+	int sum;
+	int i = 1;
+	if (n == 0)
+		return 0;
+	else
+	return (sumArray(array,n-1)+array[n-1]);
 
-    StackInt intstack(100);
-    for (int i = 0; i < len; i++)
-    {
-        char ch = s.at(i);
-        if (ch == '^' || ch == '+' || ch == '/' || ch == '*' || ch == '-')
-        {
-            int val1 = intstack.pop();
-            int val2 = intstack.pop();
-
-            switch(ch)
-            {
-            case '+':
-            {
-                    intstack.push(val1 + val2);
-                    break;
-            }
-            case '-':
-            {
-                intstack.push(val1 - val2);
-                break;
-            }
-            case '*':
-            {
-                intstack.push(val1 * val2);
-                break;
-            }
-            case '/':
-            {
-                intstack.push(val1 / val2);
-                break;
-            }
-            case'^':
-            {
-                intstack.push(pow(val2, val1));
-                break;
-            }
-
-            }
-        }
-        else
-        {
-            intstack.push((int)(s.at(i) - '0'));
-        }
-    }
-    ret = intstack.pop();
-    return(ret);
 }
 
+////Write recursive definitions of the functions whose prototypes are given in Exercises 27-29
+//29
+int location(int array[], int first, int last, int target)
+{
+	int loc = 0;
+	int i = first;
+	if (first >= last)
+	{
+		cout << "Target not found";
+		return 0;
+	}
+	if (array[first] == target)
+	{
+		return first;
+	}
+	if (array[last] == target)
+	{
+		first = last;
+		return last;
+	}
+	return location(array, ++first, --last, target);
+}
+
+//# 33 Greatest Common Divisor Program
+int GCD(int a, int b)
+{
+	a = abs(a);
+	b = abs(b);
+	if (false)
+		return 0;
+	else
+	{
+		int r = a % b;
+		if (0 == r)
+			return 0;
+		else
+		{
+			a = b;
+			b = r;
+		}
+	}
+	return b;
+}
 
 int main()
 {
-    ifstream infile;
-    string ex2;
-    infile.open("infixdata.txt");  
+	//Number 21, Write a recursive function that returns the number of digits in a nonnegative integer
+	int number, count;
+	count = 0;
+	cout << "Enter a positive number: ";
+	cin >> number;
+	count = countdigits(number);
+	cout << "There are " << count << " digits in that certain number. " << endl;
 
-        while (!infile.eof())
-        {           
-            getline(infile, ex2);           
-            string postFix1 = intopost(ex2);
-            cout << ex2 << endl;
-            cout << "Postfix : " << postFix1;
-            cout << "\nValue is : " << evaluatepost(postFix1) << endl << endl;
-        }
-        if (infile.eof())
-        {
-            cout << "End of program" << endl;
-            system("pause");
-            return 0;
-        }    
-   
+	//Number 23, Write a recursive function printReverse() that displays an integers digits in reverse order
+	int val, val2;
+	cout << "Enter digit: ";
+	cin >> val;
+	cout << "Printing out in Reverse order: ";
+	val2 = printReverse(val);
+	cout << endl;
+	//Number 27,Write recursive definitions of the functions whose prototypes are given in Exercises 27-29
+	int val3[5] = { 1,2,3,4,5 };	
+	int first = val3[0];
+	int last=val3[4];	
+	cout << "Printing out the array 1,2,3,4,5 in Reverse order: ";
+	reverseArray(val3,first,last);
+	cout << first;	
+	cout << endl;
+
+	//Number 28,Write recursive definitions of the functions whose prototypes are given in Exercises 27-29
+	int val4[5] = { 1,2,3,4,5 };
+	int n = sizeof(val4) / sizeof(val4[0]);
+	cout << "Summing up the array 1,2,3,4,5: ";
+	cout<<sumArray(val4, n);	
+	cout << endl;
+
+	//Number 29,Write recursive definitions of the functions whose prototypes are given in Exercises 27-29	
+	int n1 = sizeof(val3) / sizeof(val3[0]);
+	cout << "Searching for value 4 in array: ";
+	int x = 4;
+	int loc = location(val3, 0, n - 1, x);
+	cout << "Searching for " << x;
+	if (loc != -1)
+	{
+		cout <<"..Found it! "<< x << " is located in position " << loc;
+	}
+	else
+		cout << x << " is not present in the array!";
+
+	cout << endl;	
+
+	//Number 33, GCD program
+	while (true)
+	{
+		int a, b;
+		cout << "Enter the two values: ";
+		cin >> a >> b;
+		cout << "Greatest Common Divisor is: " << GCD(a, b) << endl;
+		char input;
+		cout << "Continue? (Y/N)";
+		cin >> input;
+		if ('y' != input)
+		{
+			break;
+		}
+	}
+
 	system("pause");
 	return 0;
 }
